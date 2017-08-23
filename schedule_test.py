fa2cf7f0d66f6e96f20aa818941c5d7a3f0a0365
arne@pi2 <arne.kaas@gmail.com>
2017-08-23 18:27:35 +0000

    updated cronjob test to save data

--- schedule_test.py
+++ schedule_test.py
@@ -4,8 +4,8 @@ import time
 def job():
 #    print"P1 read started..." + time.strftime("%Y-%m-%d %H:%M:%S")
     execfile("readp1.py")
-#    execfile("process_p1_telegram.py") 
-#    execfile("sqlite_log.py")
+    execfile("process_p1_telegram.py") 
+    execfile("sqlite_log.py")
 
 #print("Reader will launch in 60 sec")
 #time.sleep(60)
