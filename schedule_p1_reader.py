3309072d1af658d5db0ac78d0a998ca557520016
arne@pi2 <arne.kaas@gmail.com>
2017-08-23 19:49:35 +0000

    ??

--- schedule_p1_reader.py
+++ schedule_p1_reader.py
@@ -2,18 +2,12 @@
 import time
 
 def job():
-#    print"P1 read started..." + time.strftime("%Y-%m-%d %H:%M:%S")
-    execfile("readp1.py")
+#    print "P1 read started..." + time.strftime("%Y-%m-%d %H:%M:%S")
+    execfile("read_p1_telegram.py")
     execfile("process_p1_telegram.py") 
     execfile("sqlite_log.py")
 
-#print("Reader will launch in 60 sec")
-#time.sleep(60)
-#print("reader will relaunch every 5 sec")
-#schedule.every(5).seconds.do(job)
-
 while True:
-    #schedule.run_pending()
     job()
     time.sleep(1)
-#job()
+
